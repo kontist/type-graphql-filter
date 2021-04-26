f6f3bace1e514160184494b347ad9544e84b7b70
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2021-04-26 10:09:33 +0200

    Allow enums in filter

--- lib/types/metadata.ts
+++ lib/types/metadata.ts
@@ -1,14 +1,15 @@
-import { GraphQLScalarType } from "graphql";
-
+import { ClassType } from "type-graphql";
 import { FilterOperator } from ".";
+import { GraphQLScalarType } from "graphql";
+import { RecursiveArray } from "type-graphql/dist/decorators/types";
 
-export type ReturnTypeFunc = (type?: void) => GraphQLScalarType | Function;
+export type ReturnTypeFunc = (type?: void) => ClassType | GraphQLScalarType | Function | object | symbol;
 
 export type FiltersCollectionType = {
   target: Function;
   field: string | symbol;
   operators: FilterOperator[];
-  getReturnType?: ReturnTypeFunc;
+  getReturnType?: ReturnTypeFunc | RecursiveArray<ReturnTypeFunc>;
 };
 
 export type MetadataStorage = {
