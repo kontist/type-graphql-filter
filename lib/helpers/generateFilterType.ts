cd97779e8e50a7c0dc4fc484e7d645e3aebd2c1e
grund <peter.grund@bisnode.com>
2021-07-14 13:06:45 +0200

    assure we take correct model

--- lib/helpers/generateFilterType.ts
+++ lib/helpers/generateFilterType.ts
@@ -24,7 +24,8 @@ export const createFilterType = (type: Function) => {
   const typeGraphQLMetadata = getTypeGraphQLMetadataStorage();
 
   const objectTypesList = typeGraphQLMetadata.objectTypes;
-  const graphQLModel = objectTypesList.find((ot) => types.includes(ot.target) && (typeGraphQLMetadata.inputTypes.findIndex(it => it.name === (ot.name + "Filter")) < 0))
+  const graphQLModels = objectTypesList.filter((ot) => types.includes(ot.target) && (typeGraphQLMetadata.inputTypes.findIndex(it => it.name === (ot.name + "Filter")) < 0));
+  const graphQLModel = graphQLModels.find(m => m.name === type.name) || graphQLModels.slice(-1).pop()
 
   if (!graphQLModel) {
     throw new Error(
